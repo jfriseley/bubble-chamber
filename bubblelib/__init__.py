ea395fe6f0e50ce872e035102988f67da9c093bb
Jenna Riseley <jf.riseley@gmail.com>
2024-09-22 15:58:37 +1000

    Hide common rendering code in lib

--- bubblelib/__init__.py
+++ bubblelib/__init__.py
@@ -1,6 +1,37 @@
 from dataclasses import dataclass, field
-from typing import Tuple, Union
+from typing import Tuple, Union, List
+import pyvista as pv
 import numpy as np
+# from mpl_toolkits.mplot3d import Axes3D
+
+def load_paths(file_path):
+    """Loads an npz file and returns a list of lists of points."""
+    data = np.load(file_path, allow_pickle=True)
+    paths = []
+    for key in data:
+        paths.append(data[key])  
+    return paths
+
+def plot_points(points, camera_position_container):
+    """Displays the points in a PyVista plot window with a button for saving the camera position."""
+    point_cloud = pv.PolyData(points)
+    plotter = pv.Plotter()
+
+    plotter.add_mesh(point_cloud, render_points_as_spheres=True, point_size=10)
+
+    plotter.add_key_event("s", lambda: key_press_callback(plotter, 's', camera_position_container))
+    plotter.show()
+
+def key_press_callback(plotter, key, camera_position_container):
+    """Callback function triggered when a specific key is pressed."""
+    if key == 's':  # Save the camera position when 's' is pressed
+        get_camera_position(plotter, camera_position_container)
+
+def get_camera_position(plotter,camera_position_container):
+    camera_position = plotter.camera_position
+    print("Camera position:", camera_position)
+    camera_position_container[0] = camera_position
+
 
 @dataclass
 class PinholeCamera:
@@ -24,7 +55,7 @@ class PinholeCamera:
         y_c = self.up_vector 
         return x_c, y_c, z_c
     
-    def _world_to_camera(self, world_point):
+    def _world_to_camera(self, world_point) -> List[float]:
         # Convert the world point to homogeneous coordinates
 
         camera_basis = self._compute_basis()
@@ -48,13 +79,13 @@ class PinholeCamera:
         
         return list(camera_point_homogeneous[:3]/camera_point_homogeneous[3])  # Return only the 3D part
 
-    def _camera_to_image_plane(self, camera_point):
+    def _camera_to_image_plane(self, camera_point) -> Tuple[float, float]:
         x, y, z = camera_point
         image_x = -(self.focal_length * x) / z
         image_y = -(self.focal_length * y) / z
         return image_x, image_y
 
-    def _image_to_pixel(self, image_point):
+    def _image_to_pixel(self, image_point) -> Tuple[int, int]:
         image_x, image_y = image_point
         width_px, height_px = self.resolution
         pixel_x = (image_x + self.sensor_width / 2) * (width_px / self.sensor_width)
@@ -62,9 +93,20 @@ class PinholeCamera:
         return int(pixel_x), int(pixel_y)
 
     def photograph_point(self, world_point) -> Union[Tuple[int, int], None]:
-        camera_basis = self._compute_basis()
         camera_point = self._world_to_camera(world_point)
         if camera_point[2] <= 0:
             image_point = self._camera_to_image_plane(camera_point)
             pixel_coords = self._image_to_pixel(image_point)
             return pixel_coords
+
+   # if DEBUG:
+    #     camera_points = np.array(camera_points)
+    #     ax.scatter(camera_points[:, 0], camera_points[:, 1], camera_points[:, 2], color='b')    
+    #     # X axis (red)
+    #     ax.quiver(0, 0, 0, 0.01, 0, 0, color='r', arrow_length_ratio=0.1)
+    #     # Y-axis (Green)
+    #     ax.quiver(0, 0, 0, 0, 0.01, 0, color='g', arrow_length_ratio=0.1)
+    #     # Z-axis (Blue)
+    #     ax.quiver(0, 0, 0, 0, 0, 0.01, color='b', arrow_length_ratio=0.1)
+    #     plt.savefig('points_in_camera_frame')
+    #     plt.show()
--- renderers/blueprint.py
+++ renderers/blueprint.py
@@ -1,13 +1,12 @@
 import argparse
 import numpy as np
-import pyvista as pv
 import os
 from datetime import datetime
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-from bubblelib import PinholeCamera
+
+from bubblelib import PinholeCamera, load_paths, plot_points
 
 DEBUG = False
 SENSOR_WIDTH = 0.0036#0.036  # Full-frame sensor width
@@ -15,35 +14,6 @@ SENSOR_HEIGHT = 0.0024#0.024  # Full-frame sensor height
 HORIZONTAL_RESOLUTION = 3600
 OUTPUT_DIR='out'
 
-def load_npz_file(file_path):
-    """Loads an npz file and returns a list of lists of points."""
-    data = np.load(file_path, allow_pickle=True)
-    paths = []
-    for key in data:
-        paths.append(data[key])  
-    return paths
-
-def get_camera_position(plotter,camera_position_container):
-    camera_position = plotter.camera_position
-    print("Camera position:", camera_position)
-
-    camera_position_container[0] = camera_position
-
-
-def key_press_callback(plotter, key, camera_position_container):
-    """Callback function triggered when a specific key is pressed."""
-    if key == 's':  # Save the camera position when 's' is pressed
-        get_camera_position(plotter, camera_position_container)
-
-def plot_points(points, camera_position_container):
-    """Displays the points in a PyVista plot window with a button for saving the camera position."""
-    point_cloud = pv.PolyData(points)
-    plotter = pv.Plotter()
-
-    plotter.add_mesh(point_cloud, render_points_as_spheres=True, point_size=10)
-
-    plotter.add_key_event("s", lambda: key_press_callback(plotter, 's', camera_position_container))
-    plotter.show()
 
 def interpolate_path(path, num_points=10):
     # Interpolate the path to create smooth transitions
@@ -66,7 +36,7 @@ if __name__ == "__main__":
     parser.add_argument("file", type=str, help="Path to the npz file containing the points")
     args = parser.parse_args()
 
-    paths = load_npz_file(args.file)
+    paths = load_paths(args.file)
 
     camera_position_container = [None]
 
@@ -102,25 +72,10 @@ if __name__ == "__main__":
                 rasterised_points.append(rasterised_point)
 
 
-    # if DEBUG:
-    #     camera_points = np.array(camera_points)
-    #     ax.scatter(camera_points[:, 0], camera_points[:, 1], camera_points[:, 2], color='b')    
-    #     # X axis (red)
-    #     ax.quiver(0, 0, 0, 0.01, 0, 0, color='r', arrow_length_ratio=0.1)
-    #     # Y-axis (Green)
-    #     ax.quiver(0, 0, 0, 0, 0.01, 0, color='g', arrow_length_ratio=0.1)
-    #     # Z-axis (Blue)
-    #     ax.quiver(0, 0, 0, 0, 0, 0.01, color='b', arrow_length_ratio=0.1)
-    #     plt.savefig('points_in_camera_frame')
-    #     plt.show()
-
-    
     crisp_radius = 2
     for point in rasterised_points:
         clear = (point[0], point[1])
-
-        # Draw multiple circles for fuzziness
-        draw.ellipse([clear[0]-crisp_radius, clear[1]-crisp_radius, clear[0]+crisp_radius, clear[1]+crisp_radius], fill=(230, 255, 255, 128))
+        draw.ellipse([clear[0]-crisp_radius, clear[1]-crisp_radius, clear[0]+crisp_radius, clear[1]+crisp_radius], fill=(230, 255, 255, 200))
         
     for point in rasterised_points:
         fuzzy_radius = int(np.random.uniform(1,5))
